86bf212acae75f0f255e9105f48c57b523f05620
Slava <plumsoftwareofficial@gmail.com>
2024-01-13 12:31:56 +0600

    [+] added new theme and new colors [+] added Crossfade to change app theme with animations [+] added vibrator permission in manifest

--- app/src/main/java/ru/plumsoftware/coffeeapp/MainActivity.kt
+++ app/src/main/java/ru/plumsoftware/coffeeapp/MainActivity.kt
@@ -3,44 +3,55 @@ package ru.plumsoftware.coffeeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.Crossfade
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import ru.plumsoftware.coffeeapp.ui.theme.БаристаКофеИНапиткиTheme
+import com.example.compose.CoffeeAppTheme
+import com.example.compose.LightColors
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            БаристаКофеИНапиткиTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+
+            val targetColor by remember {
+                mutableStateOf(LightColors)
+            }
+            val systemUiController = rememberSystemUiController()
+            val color = targetColor.background
+
+            Crossfade(
+                targetState = targetColor,
+                animationSpec = tween(400),
+                label = "change theme"
+            ) { colorScheme ->
+
+                SideEffect {
+                    systemUiController.setStatusBarColor(
+                        color = color,
+                    )
+                    systemUiController.setNavigationBarColor(
+                        color = color
+                    )
+                }
+
+                CoffeeAppTheme(colors = colorScheme) {
+                    Surface(
+                        modifier = Modifier.fillMaxSize(),
+                        color = MaterialTheme.colorScheme.background
+                    ) {
+
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    БаристаКофеИНапиткиTheme {
-        Greeting("Android")
     }
 }
